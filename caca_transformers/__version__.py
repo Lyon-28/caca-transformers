6f036af992ae74c2a922c6e0186bbbae5154d58f
Lyon <122740857+Lyon-28@users.noreply.github.com>
2025-12-05 09:19:34 +0700

    Update __version__.py

--- caca_transformers/__version__.py
+++ caca_transformers/__version__.py
@@ -1,6 +1,6 @@
 """Informasi versi package"""
 
-__version__ = "1.0.0"
+__version__ = "0.0.1"
 __author__ = "Lyon"
 __email__ = "cacatransformers@gmail.com"
 __description__ = "Arsitektur Transformer Modern dengan GQA, RoPE, SwiGLU & Flash Attention"
